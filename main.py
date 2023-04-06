13cef47df9d65217cf70d0bbadab68a7d0d71b12
Manonp59 <manon-platteau@orange.fr>
2023-04-06 13:59:53 +0200

    maj

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ import pandas as pd
 
 
 
-url = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=gc_obo_gare_origine_r_name+%3D+%22Paris%22+AND+(date%3D'2019'+OR+date%3D'2020'+OR+date+%3D'2021'+OR+date%3D'2022')&sort=date&facet=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c&timezone=Europe%2FParis"
-
+url = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=gc_obo_gare_origine_r_name+%3D+%22Paris%22+AND+(date%3D'2019'+OR+date%3D'2020'+OR+date+%3D'2021'+OR+date%3D'2022')&sort=date&facet=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c&timezone=Europe%2FParis&rows=-1"
 
 
 response = requests.get(url)
-
-if response.ok:
-    data = response.json()
-    print(data)
-else:
-    print('Erreur lors de la requête :', response.status_code)
-    
-df_data = pd.DataFrame(data, columns=[
-    "date",
-    "gc_obo_date_heure_restitution_c",
-    "gc_obo_gare_origine_r_name",
-    "gc_obo_nature_c",
-    "gc_obo_type_c",
-    "gc_obo_nom_recordtype_sc_c"
-])
-
-df_data.to_csv('df_data.csv')
+data = response.json()
+facet = data["parameters"]["facet"]
 
 
+#---------------------------------------------------------------------------------------------------------------
+# Extraire les valeurs de facet
+facets = facet
+date = facets[0]
+gc_obo_date_heure_restitution_c = facets[1]
+gc_obo_gare_origine_r_name = facets[2]
+gc_obo_nature_c = facets[3]
+gc_obo_type_c = facets[4]
+gc_obo_nom_recordtype_sc_c = facets[5]
 
-url_gares = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&q=&sort=gare_alias_libelle_noncontraint&facet=latitude_entreeprincipale_wgs84&facet=longitude_entreeprincipale_wgs84"
-response = requests.get(url)
+# Extraire les champs des enregistrements
+records = data["records"]
+record_fields = []
+for record in records:
+    record_fields.append(record["fields"])
 
-if response.ok:
-    data = response.json()
-    print(data)
-else:
-    print('Erreur lors de la requête :', response.status_code)
-    
+#print(record_fields)
+
+# Créer un DataFrame à partir des champs des enregistrements
+df = pd.DataFrame(record_fields)
+print(df)
+
+
+# Écrire le DataFrame dans un fichier CSV
+df.to_csv("objets-trouves.csv", index=False)
+
 
 
 
