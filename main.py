69e5138b5af29ccfec110ae26cabc1dd11460419
Manonp59 <manon-platteau@orange.fr>
2023-04-06 13:55:29 +0200

    table coordonnées gares

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-api_key = 'YOUR_API_KEY_HERE' # Remplacez YOUR_API_KEY_HERE par votre clé d'API SNCF
+
 
 url = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=gc_obo_gare_origine_r_name+%3D+%22Paris%22+AND+(date%3D'2019'+OR+date%3D'2020'+OR+date+%3D'2021'+OR+date%3D'2022')&sort=date&facet=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c&timezone=Europe%2FParis"
 
@@ -25,6 +25,17 @@ df_data = pd.DataFrame(data, columns=[
 ])
 
 df_data.to_csv('df_data.csv')
+
+
+
+url_gares = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&q=&sort=gare_alias_libelle_noncontraint&facet=latitude_entreeprincipale_wgs84&facet=longitude_entreeprincipale_wgs84"
+response = requests.get(url)
+
+if response.ok:
+    data = response.json()
+    print(data)
+else:
+    print('Erreur lors de la requête :', response.status_code)
     
 
 
